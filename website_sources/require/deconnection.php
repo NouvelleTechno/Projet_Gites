22a2f110372d090cde4bd4efc31fe4f726661241
Yoann Génique <genique.yoann@outlook.com>
2020-12-21 10:31:08 +0100

    maj

--- website_sources/require/deconnection.php
+++
@@ -1,9 +0,0 @@
-<?php 
-// Démarre la session
-session_start();
-// Détruit la session(deconnect)
-session_destroy();
-// redirige vers la page de login
-header("location:../admin/index.php");
-exit(); 
-?> 
